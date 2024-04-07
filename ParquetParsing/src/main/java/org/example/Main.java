047b87841fde5e0065967e84ab13e5bbf4d22d5f
Kihun SONG <kihunbuzz@naver.com>
2024-04-07 20:28:48 +0900

    Parquet parse

--- ParquetParsing/src/main/java/org/example/Main.java
+++ ParquetParsing/src/main/java/org/example/Main.java
@@ -1,15 +1,16 @@
 package org.example;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.avro.generic.GenericRecord;
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
 import org.apache.parquet.avro.AvroParquetReader;
 import org.apache.parquet.hadoop.ParquetReader;
 import org.apache.parquet.hadoop.util.HadoopInputFile;
-import org.apache.hadoop.fs.Path;
 import org.apache.parquet.io.InputFile;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.IOException;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -25,22 +26,31 @@ public class Main {
 
         GenericRecord record;
         while ((record = parquetReader.read()) != null) {
+            String recordSchema = record.getSchema().toString(true);
+
             // Skip the Avro schema by directly accessing the record's fields
             String continentString = record.get("continent").toString();
-            String countryString = record.get("country").toString();
+
+            GenericRecord countryRecord = (GenericRecord) record.get("country");
+            String countryString = countryRecord.toString();
+
+            String nameString = countryRecord.get("name").toString();
+
+            @SuppressWarnings("unchecked") // 타입 경고 억제
+            List<GenericRecord> cityRecord = (List<GenericRecord>) countryRecord.get("city");
+            for(GenericRecord city : cityRecord) {
+                System.out.print(city.get("array_element") + ", ");
+            }
+            System.out.println("----------------------------------------");
 
             Country country = objectMapper.readValue(countryString, Country.class);
             Cities cities = new Cities();
             cities.continent = continentString;
             cities.country = country;
-
-            System.out.println(cities);
+//            System.out.println(cities);
         }
 
         parquetReader.close();
-
-        // ParquetToJson parquetToJson = new ParquetToJson(filePath);
-        // parquetToJson.convertToJson();
 
     }
 }
--- ParquetParsing/src/main/java/org/example/ParquetToJson.java
+++ ParquetParsing/src/main/java/org/example/ParquetToJson.java
@@ -2,17 +2,12 @@ package org.example;
 
 import java.io.IOException;
 
+import org.apache.avro.generic.GenericRecord;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
-import org.apache.parquet.example.data.Group;
-import org.apache.parquet.example.data.simple.SimpleGroup;
-import org.apache.parquet.example.data.simple.convert.GroupRecordConverter;
-import org.apache.parquet.hadoop.ParquetFileReader;
+import org.apache.parquet.avro.AvroParquetReader;
 import org.apache.parquet.hadoop.ParquetReader;
-import org.apache.parquet.hadoop.example.GroupReadSupport;
 import org.apache.parquet.hadoop.util.HadoopInputFile;
-import org.apache.parquet.io.InputFile;
-import org.apache.parquet.schema.MessageType;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -25,28 +20,18 @@ public class ParquetToJson {
     }
 
     public void convertToJson() throws IllegalArgumentException, IOException {
-        // Code to convert Parquet file to JSON
-        InputFile inputFile = HadoopInputFile.fromPath(new Path(filePath), new Configuration());
-        try (ParquetFileReader reader = ParquetFileReader.open(inputFile)) {
-            MessageType schema = reader.getFooter().getFileMetaData().getSchema();
-            GroupReadSupport groupReadSupport = new GroupReadSupport();
-            ParquetReader<Group> groupReader = ParquetReader.builder(groupReadSupport, new Path(filePath)).build();
+        ObjectMapper objectMapper = new ObjectMapper();
 
-            GroupRecordConverter converter = new GroupRecordConverter(schema);
-            ObjectMapper objectMapper = new ObjectMapper();
+        // Parquet 파일 읽기
+        ParquetReader<GenericRecord> reader = AvroParquetReader
+                .<GenericRecord>builder(HadoopInputFile.fromPath(new Path(filePath), new Configuration()))
+                .build();
 
-            Group group;
-            while ((group = groupReader.read()) != null) {
-                SimpleGroup simpleGroup = (SimpleGroup) converter.getCurrentRecord();
-                String json = objectMapper.writeValueAsString(simpleGroup);
-                System.out.println(json);
-
-            }
-
-        } catch (Exception e) {
-            System.out.println("dead");
-            System.out.println(e.getMessage());
+        GenericRecord record;
+        while ((record = reader.read()) != null) {
+            // GenericRecord를 JSON 문자열로 변환
+            String jsonString = objectMapper.writeValueAsString(record);
+            System.out.println(jsonString);
         }
-
     }
 }
